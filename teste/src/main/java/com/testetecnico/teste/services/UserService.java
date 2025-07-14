a32730657da9ff87b45726af2415cdcf16715ad8
Aser Junior <61999214+aserjunior@users.noreply.github.com>
2025-07-14 16:51:30 -0300

    Service layer and Component registration

---
+++ teste/src/main/java/com/testetecnico/teste/services/UserService.java
@@ -0,0 +1,27 @@
+package com.testetecnico.teste.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.testetecnico.teste.entities.User;
+import com.testetecnico.teste.repositories.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findBindId(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
